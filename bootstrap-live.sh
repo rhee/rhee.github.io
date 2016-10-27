7380d2df20189b5ac34779f9c0b2f93505dab830
Sang-Hoon RHEE <rhee@users.noreply.github.com>
2016-10-27 07:39:14 +0000

    Update bootstrap-live.sh

--- bootstrap-live.sh
+++ bootstrap-live.sh
@@ -1,45 +1,39 @@
 #!/bin/sh
 # bootstrap environment for ubuntu 14.04 live usb 
 
-# connect to KT_WLAN_9921
-echo -n "Enter wifi key:" 1>&2
-read wlan_key
-nmcli d wifi connect KT_WLAN_9921 password $wlan_key iface wlan0
+##########################################
+# system setup part
+sudo -H sh -c '
 
 # enable universe repository apt get update, install extra packages
-sudo -H add-apt-repository universe &&
-#sudo -H add-apt-repository multiverse &&
-sudo -H apt-get -y update &&
-sudo -H apt-get -y install vim-gnome exuberant-ctags git
+add-apt-repository universe && 
+apt-get -y update && 
+apt-get -y install vim-gnome exuberant-ctags git python-pip
 
-sudo -H apt-get -y -f libav-tools python-pip python-virtualenv
+# install vscode
+wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O /tmp/vscode.deb
+#dpkg -i /tmp/vscode.deb
+
+
+# enable google chrome install
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 
 
 # fix locale
-sudo -H locale-gen ko-KR.UTF-8
-sudo -H update-locale
+locale-gen ko-KR.UTF-8
+update-locale
 export LANG=ko_KR.UTF-8
 
-# install node manager
+' # end of sudo -H sh -c
+
+##########################################
+# user setup part
+
+# user binary path
 mkdir -p ~/.local/bin
-cat <<EEE >> ~/.bashrc
-export PATH=~/.local/bin:$PATH
+PATH=~/.local/bin:$PATH
 alias i='. ~/.ishrc'
-EEE
-
-#
-. ~/.bashrc
-
-# install n, select es2015-supported latest stable node.js
-( git clone https://github.com/rhee/n.git && cd ~/n && make PREFIX=~/.local install && hash -r && n stable )
-
-
-# install default .rcfiles
-(mkdir -p ~/.local/etc; cd ~/.local/etc; git clone https://github.com/rhee/_ishrc.git _ishrc; cd _ishrc; sh install.sh)
-
-# install vscode
-wget 'https://go.microsoft.com/fwlink/?LinkID=760868' -O vscode.deb
-sudo -H dpkg -i vscode.deb
 
 
 # disable touchpad
@@ -47,6 +41,21 @@ synclient TouchpadOff=1
 
 # remap key capslock as control
 setxkbmap -layout us -option ctrl:nocaps
+
+
+# install node manager
+cat <<EEE >> ~/.bashrc
+export PATH=~/.local/bin:$PATH
+alias i='. ~/.ishrc'
+EEE
+
+
+# install n, select es2015-supported latest stable node.js
+git clone https://github.com/rhee/n.git && ( cd ~/n && make PREFIX=~/.local install; hash -r; n stable )
+
+
+# install default .rcfiles
+git clone https://github.com/rhee/_ishrc.git && ( cd _ishrc; sh install.sh )
 
 
 
