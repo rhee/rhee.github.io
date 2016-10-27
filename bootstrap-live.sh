6b40fb01ea260d5943992f6edd86f36dbad140e3
Sang-Hoon RHEE <rhee@users.noreply.github.com>
2016-10-27 07:13:34 +0000

    Update bootstrap-live.sh

--- bootstrap-live.sh
+++ bootstrap-live.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 # bootstrap environment for ubuntu 14.04 live usb 
 
-# disable touchpad
-synclient TouchpadOff=1
-
-# remap key capslock as control
-setxkbmap -layout us -option ctrl:nocaps
-
 # connect to KT_WLAN_9921
 echo -n "Enter wifi key:" 1>&2
 read wlan_key
@@ -27,21 +21,30 @@ sudo -H update-locale
 export LANG=ko_KR.UTF-8
 
 # install node manager
-mkdir -p ~/.local/bin ~/.local/etc/profile.d
+mkdir -p ~/.local/bin
 cat <<EEE >> ~/.bashrc
 export PATH=~/.local/bin:$PATH
 alias i='. ~/.ishrc'
 EEE
 . ~/.bashrc
-git clone https://github.com/rhee/n.git
-(cd ~/n; make PREFIX=~/.local install)
-n stable # fetch latest (es2015 supported) node.js
+
+# install n, select es2015-supported latest stable node.js
+( git clone https://github.com/rhee/n.git && cd ~/n && make PREFIX=~/.local install && harh -r && n stable )
+
 
 # install default .rcfiles
-mkdir -p ~/.local/etc
-(cd ~/.local/etc; git clone https://github.com/rhee/_ishrc.git _ishrc; cd _ishrc; sh install.sh)
+(mkdir -p ~/.local/etc; cd ~/.local/etc; git clone https://github.com/rhee/_ishrc.git _ishrc; cd _ishrc; sh install.sh)
 
 # install vscode
 wget 'https://go.microsoft.com/fwlink/?LinkID=760868' -O vscode.deb
 sudo -H dpkg -i vscode.deb
+
+
+# disable touchpad
+synclient TouchpadOff=1
+
+# remap key capslock as control
+setxkbmap -layout us -option ctrl:nocaps
+
+
 
